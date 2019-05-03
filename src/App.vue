98cfdac9813b8edba7f5f854b20c6f5f33510c88
Louis Eveillard <barlou15+github@gmail.com>
2019-05-03 10:52:35 +0200

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Sketch />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Sketch from "./components/Sketch.vue";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    Sketch
   }
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+body {
+  font-family: sans-serif;
+
+  line-height: 1.5;
+  background-color: white;
+  color: black;
+
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
